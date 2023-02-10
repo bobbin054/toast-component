c30bc1c7e17e126b9827a5059caa9c0bca7d5086
Joakim Östelid <joakim.ostelid@outlook.com>
2023-02-10 22:28:00 +0100

    aa

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import ToastPlayground from '../ToastPlayground';
-import Footer from '../Footer';
+import ToastPlayground from "../ToastPlayground";
+import Footer from "../Footer";
 
 function App() {
+  const a = 1;
   return (
     <>
       <ToastPlayground />
